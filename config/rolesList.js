1cea1c812372df04bbc1323abf8ed25b9396d69e
devansh19jain <devanshjain1910@gmail.com>
2024-11-25 19:29:13 +0530

    Initial Commit

---
+++ config/rolesList.js
@@ -0,0 +1,6 @@
+const ROLES_LIST = {
+    "User": 2020,
+    "Admin": 2021,
+    "Moderator" : 2022,
+}
+module.exports = ROLES_LIST
---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require('path');
+const cors = require('cors');
+const http = require('http'); 
+
+const app = express();
+const corsOptions = require('./config/corsOptions');
+const credentials = require('./middleware/credentials');
+const errorHandler = require('./middleware/errorHandler');
+const cookieParser = require('cookie-parser');
+
+const PORT = process.env.PORT || 8080;
+
+// Create an HTTP server
+const server = http.createServer(app);
+
+// for chrome related issues in cors
+app.use(credentials);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json 
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(errorHandler);
+
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
